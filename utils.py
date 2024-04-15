7c073de5b22e2dbd131a734fe70e1b79980b414b
hosein-fanai <hosein.fanai@gmail.com>
2024-04-16 00:42:44 +0330

    RL algorithms won't be bothered by pygame.

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from wrappers.normalize_observation import NormalizeObservation
 
 def make_env(max_steps=30, size=(5, 5), orb_num=5, pit_num=5, seed=None, 
             onehot_obs=True, norm_obs=False, num_stack=None):
-    env = PitsAndOrbsEnv(max_steps=max_steps, size=size, orb_num=orb_num, pit_num=pit_num, seed=seed)
+    env = PitsAndOrbsEnv(pygame_mode=False, max_steps=max_steps, size=size, orb_num=orb_num, pit_num=pit_num, seed=seed)
 
     if onehot_obs and not norm_obs:
         env = OnehotObservation(env)
