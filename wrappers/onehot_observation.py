e2adc8f9c83dcd36aa11ecd3e32e5527da456712
hosein-fanai <hosein.fanai@gmail.com>
2024-04-27 02:48:13 +0330

    Fixed issues with onehoting player_movements.

--- wrappers/onehot_observation.py
+++ wrappers/onehot_observation.py
@@ -14,7 +14,7 @@ class OnehotObservation(gym.ObservationWrapper):
         players_num = self.env.game.players_num
         self.observation_space = spaces.Dict({
             "board": spaces.Box(low=0, high=1, shape=(*self.env.game.size, len(self.env.game.CELLS)), dtype=np.uint8), 
-            **{f"player{i}_movements": spaces.Box(low=0., high=1., shape=(self.env.max_movements,), dtype=np.float32) for i in range(players_num)}, 
+            **{f"player{i}_movements": spaces.Box(low=0., high=1., shape=(self.env.max_movements+1,), dtype=np.float32) for i in range(players_num)}, 
             **{f"player{i}_direction": spaces.Box(low=0, high=1, shape=(4,), dtype=np.uint8) for i in range(players_num)}, 
             **{f"player{i}_has_orb": spaces.Box(low=0, high=1, shape=(1,), dtype=np.uint8) for i in range(players_num)} | 
             ({f"player{i}_position": spaces.Box(low=0, high=1, shape=(2, max(self.env.game.size)), dtype=np.uint8) for i in range(players_num)} if players_num > 1 else {}) | # TODO: make it support rectangular grids
@@ -26,7 +26,7 @@ class OnehotObservation(gym.ObservationWrapper):
 
         return OrderedDict([
             ("board", self._onehot(observation["board"], depth=len(self.env.game.CELLS))), 
-            *((f"player{i}_movements", self._onehot(np.array(observation[f"player{i}_movements"]), depth=self.env.max_movements)) for i in range(players_num)), 
+            *((f"player{i}_movements", self._onehot(np.array(observation[f"player{i}_movements"]), depth=self.env.max_movements+1)) for i in range(players_num)), 
             *((f"player{i}_direction", self._onehot(np.array(observation[f"player{i}_direction"]), depth=4)) for i in range(players_num)), 
             *((f"player{i}_has_orb", np.array([observation[f"player{i}_has_orb"]])) for i in range(players_num))] + 
             ([(f"player{i}_position", self._onehot(observation[f"player{i}_position"], depth=max(self.env.game.size))) for i in range(players_num)] if players_num > 1 else []) + 
