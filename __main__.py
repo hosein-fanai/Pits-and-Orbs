393c81cafce4e9fd03d68b8d5b1a994b09e2512f
hosein-fanai <hosein.fanai@gmail.com>
2024-04-27 18:16:46 +0330

    Improved CLI usage.

--- __main__.py
+++ __main__.py
@@ -3,18 +3,17 @@ from argparse import ArgumentParser
 from agent.agent import Agent
 
 
-parser = ArgumentParser(description="A multi-agent-system project (called Pits and Orbs) which is able to be ran via cli.")
-parser.add_argument("--file", help="The config file path for previuos flag.")
+parser = ArgumentParser(description="A multi-agent-system project (called Pits and Orbs) which is able to be used via cli.")
 parser.add_argument("--run", "-r", action="store_true", help="The argument to run the agent on the environment \
                     with the given config file (can't be used with train flag on).")
 parser.add_argument("--train", "-t", action="store_true", help="The argument to train the agent with the specified \
                     with the given config file(can't be used with run flag on).")
-
+parser.add_argument("config", help="The config file path for previuos flag.")
 
 args = parser.parse_args()
-config_file_path = args.file
 run_flag = args.run
 train_flag = args.train
+config_file_path = args.config
 
 assert not(run_flag and train_flag) and (run_flag or train_flag)
 
