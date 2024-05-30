4481e8ce4bc0507da82eb13b5bd2f9d86d97fde6
hosein-fanai <hosein.fanai@gmail.com>
2024-05-30 16:44:36 +0330

    Refactored agent.py and added hard_coded ploicy for 1v1 mode.

--- rl/agent.py
+++ rl/agent.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import yaml
 
 import time
@@ -15,21 +17,241 @@ from utils import make_env
 from wrappers.self_play_wrapper import SelfPlayWrapper
 
 
+class Model:
+
+    def __init__(self, pred_func):
+        self.pred_func = pred_func
+
+    def predict(self, obs, deterministic=True):
+        return self.pred_func(obs), None
+
+
 class Agent:
 
-    def __init__(self, model_path=None, model=None):
+    def __init__(self, model_path=None, algorithm=None, model=None, env=None):
         assert model_path is None or model is None
 
-        self.drop_model()
-
         if model_path:
-            self.load_model(model_path)
+            assert algorithm is not None
 
-        if model:
-            self.model = model
+            self.load_model(model_path, algorithm)
 
-    def run_rules_one_step(self, obs):
+        self.model = model
+        self.env = env
+
+    def _calc_manhattan_dist(self, pos1, pos2):
+        dist = abs(pos1[0] - pos2[0])
+        dist += abs(pos1[1] - pos2[1])
+
+        return dist
+
+    def _find_closest_empty_pit(self, board, current_pos):
+        current_pos_to_pit_pos_dists = []
+        pit_pos_list = []
+        for pit_cell in (3, 5): # for all not-filled pits
+            pit_pos_Is, pit_pos_Js = np.where(board == pit_cell)
+            if len(pit_pos_Is) == 0:
+                continue
+
+            for pit_pos in zip(pit_pos_Is, pit_pos_Js):
+                pit_pos_list.append(pit_pos)
+                current_pos_to_pit_pos_dists.append(
+                    self._calc_manhattan_dist(current_pos, pit_pos)
+                )
+
+        pit_pos_and_dists = list(zip(pit_pos_list, current_pos_to_pit_pos_dists))
+        pit_pos_and_dists = sorted(pit_pos_and_dists, key=lambda x: x[1])
+
+        if len(pit_pos_and_dists) > 0:
+            closest_pit_pos = pit_pos_and_dists[0]
+        else:
+            closest_pit_pos = None, None
+
+        return closest_pit_pos
+
+    def _optimal_orb_choice_from_agent_to_pits_dist(self, board, player_pos):
+        min_dists = []
+        orb_pos_list = []
+        for orb_cell in (2, 4): # for all orbs
+            orb_pos_Is, orb_pos_Js = np.where(board == orb_cell)
+            if len(orb_pos_Is) == 0:
+                continue
+
+            for orb_pos in zip(orb_pos_Is, orb_pos_Js):
+                orb_pos_list.append(orb_pos)
+                agent_to_orb_dist = self._calc_manhattan_dist(orb_pos, player_pos)
+                _, min_orb_to_pit_dist = self._find_closest_empty_pit(board, orb_pos)
+
+                if min_orb_to_pit_dist is None:
+                    return None
+
+                min_dists.append(agent_to_orb_dist+min_orb_to_pit_dist)
+
+        orb_pos_and_min_dists = list(zip(orb_pos_list, min_dists))
+        orb_pos_and_min_dists = sorted(orb_pos_and_min_dists, key=lambda x: x[1])
+
+        if len(orb_pos_and_min_dists) > 0:
+            optimal_orb_pos = orb_pos_and_min_dists[0][0]
+        else:
+            optimal_orb_pos = None
+
+        return optimal_orb_pos
+
+    def _calc_direction(self, destination_pos, player_pos):
+        i_diff = player_pos[0] - destination_pos[0]
+        j_diff = player_pos[1] - destination_pos[1]
+
+        if abs(i_diff) > abs(j_diff):
+            if i_diff < 0:
+                return 3
+            elif i_diff > 0:
+                return 1
+        else:
+            if j_diff < 0:
+                return 2
+            elif j_diff > 0:
+                return 0
+
+    def _change_direction_to(self, target_direction, player_direction):
+        if player_direction == target_direction:
+            return 1
+
+        return 0
+
+    def _explore_board(self, board, player_pos, player_direction, do_random=False):
+        board_shape = board.shape
+
+        board_center = board_shape[0]//2, board_shape[1]//2
+        board_corner0 = 0, 0
+        board_corner1 = 0, board_shape[1]-1
+        board_corner2 = board_shape[0]-1, 0
+        board_corner3 = board_shape[0]-1, board_shape[1]-1
+
+        proposed_destinations = (board_center, board_corner0, 
+                                board_corner1, board_corner2, 
+                                board_corner3)
+        if not do_random:
+            dists = []
+            for proposed_destination in proposed_destinations:
+                distance = self._calc_manhattan_dist(player_pos, proposed_destination)
+                dists.append(distance)
+
+            destination_pos_and_dists = list(zip(proposed_destinations, dists))
+            destination_pos_and_dists = sorted(destination_pos_and_dists, key=lambda x: x[1])
+            max_dist_destination_pos = destination_pos_and_dists[-1][0]
+        else:
+            max_dist_destination_pos = random.choice(proposed_destinations)
+
+        target_direction = self._calc_direction(max_dist_destination_pos, player_pos)
+        action = self._change_direction_to(target_direction, player_direction)
+        return action
+
+    def _find_other_player_pos(self, board, player_pos):
+        for player_cell in (1, 4, 5, 7): # for all players
+            player_pos_Is, player_pos_Js = np.where(board == player_cell)
+            if len(player_pos_Is) == 0:
+                continue
+
+            for other_player_pos in zip(player_pos_Is, player_pos_Js):
+                if other_player_pos[0] != player_pos[0] or other_player_pos[1] != player_pos[1]:
+                    return other_player_pos
+
         return None
+
+    def _check_players_collision(self, board, player_pos):
+        other_player_pos = self._find_other_player_pos(board, player_pos)
+        if other_player_pos is not None:
+            dist = self._calc_manhattan_dist(other_player_pos, player_pos)
+        else:
+            return False
+
+        if dist in (1, 2):
+            return True
+
+        return False
+
+    def _find_closest_other_filled_pit(self, board, player_pos, filled_pits_positions):
+        other_pit_pos_list = []
+        dists = []
+
+        pit_pos_Is, pit_pos_Js = np.where(board == 7)
+        if len(pit_pos_Is) == 0:
+            return None, None
+
+        for pit_pos in zip(pit_pos_Is, pit_pos_Js):
+            if pit_pos not in filled_pits_positions:
+                other_pit_pos_list.append(pit_pos)
+                dists.append(self._calc_manhattan_dist(player_pos, pit_pos))
+
+        pit_pos_and_dists = list(zip(other_pit_pos_list, dists))
+        pit_pos_and_dists = sorted(pit_pos_and_dists, key=lambda x: x[1])
+
+        if len(pit_pos_and_dists) > 0:
+            closest_pit_pos = pit_pos_and_dists[0]
+        else:
+            closest_pit_pos = None, None
+
+        return closest_pit_pos
+
+    def _1v1_rules_function(self, obs):
+        board = obs["board"]
+        player_direction = obs["player0_direction"]
+        player_has_orb = obs["player0_has_orb"]
+        player_pos = obs["player0_position"]
+        filled_pits_positions = obs["filled_pits_positions"]
+
+        if self._check_players_collision(board, player_pos):
+            action = self._explore_board(board, player_pos, player_direction, do_random=True)
+            return action
+
+        closest_other_filled_pit_pos, dist_to_closest_other_filled_pit = self._find_closest_other_filled_pit(board, player_pos, filled_pits_positions)
+        if closest_other_filled_pit_pos is not None and dist_to_closest_other_filled_pit < 14:
+            target_direction = self._calc_direction(closest_other_filled_pit_pos, player_pos)
+            action = self._change_direction_to(target_direction, player_direction)
+            return action
+
+        if board[player_pos[0], player_pos[1]] == 7:
+            if player_pos not in filled_pits_positions:
+                return 4
+
+        if player_has_orb:
+            closest_pit_pos, _ = self._find_closest_empty_pit(board, player_pos)
+            if closest_pit_pos is None:
+                action = self._explore_board(board, player_pos, player_direction)
+                return action
+
+            if closest_pit_pos[0] == player_pos[0] and closest_pit_pos[1] == player_pos[1]:
+                return 3
+            
+            target_direction = self._calc_direction(closest_pit_pos, player_pos)
+            action = self._change_direction_to(target_direction, player_direction)
+            return action
+
+        optimal_orb_pos = self._optimal_orb_choice_from_agent_to_pits_dist(board, player_pos)
+        if optimal_orb_pos is None:
+            action = self._explore_board(board, player_pos, player_direction)
+            return action
+        
+        if optimal_orb_pos[0] == player_pos[0] and optimal_orb_pos[1] == player_pos[1]:
+            return 2
+        
+        target_direction = self._calc_direction(optimal_orb_pos, player_pos)
+        action = self._change_direction_to(target_direction, player_direction)
+        return action
+
+    def run_rules_one_step(self, obs, rule_type):
+        match rule_type:
+            case "1v1":
+                return self._1v1_rules_function(obs)
+            case _:
+                raise NotImplemented
+
+    def convert_rules_to_model(self, rule_type):
+        match rule_type:
+            case "1v1":
+                self.model = Model(self._1v1_rules_function)
+            case _:
+                raise NotImplemented
 
     def run_agent_one_episode(self, env, max_steps=500, fps=None, 
                 deterministic_action_choice=False, 
@@ -38,22 +260,20 @@ class Agent:
 
         obs = env.reset()
         done = False
-        rewards = 0.
 
+        rewards = []
         frames = []
-        frames_counter = 0
-
         start = time.time()
-        for step in range(max_steps):
+
+        for step in range(1, max_steps+1):
             action = self.predict(obs, deterministic=deterministic_action_choice)
 
             obs, reward, done, info = env.step(action)
 
-            rewards += reward
-            frames_counter += 1
+            rewards.append(reward)
 
             if print_rewards:
-                print(f"\rTotal Rewards: {rewards:.4f}, Current Reward: {reward:.4f}, Taken Action: {action}", end="")
+                print(f"\rTotal Rewards: {sum(rewards):.4f}, Current Reward: {reward:.4f}, Taken Action: {action}", end="")
 
             if return_frames:
                 frame = env.render("rgb_array")
@@ -72,15 +292,20 @@ class Agent:
                 env.game.clock.tick(fps)
 
         print()
-        print("Total rewards were:", rewards)
-        print("Final info:", info)
-        print("The episode was fps:", int(frames_counter/(time.time()-start)))
-        print("The episode length was:", step+1)
+
+        if not print_rewards:
+            print("Total Rewards:", sum(rewards))
+
+        print("Final Info:", info)
+        print("Episode Length:", step+1)
+        print()
+        print("Episode FPS:", int(step/(time.time()-start)))
 
         return rewards, frames
 
-    def train_with_self_play(self, env, clone_model, models_bank_maxlen=5, self_play_epochs=20, 
-                            iterations=5_000_000, log_interval=1_000, models_bank_path="./models/self-play", 
+    def train_with_self_play(self, env, clone_model, models_bank_maxlen=5, 
+                            self_play_epochs=20, iterations=5_000_000, 
+                            log_interval=1_000, models_bank_path="./models/self-play", 
                             algorithm="A2C"):
         if not isinstance(env, SelfPlayWrapper):
             try: # if the env is a sb3-based env
@@ -151,6 +376,7 @@ class Agent:
         print('*')
         print("* Loaded configs from the file are:")
         print(configs)
+        print()
 
         algorithm = list(configs.keys())[0].upper()
         configs = configs[algorithm]
@@ -221,6 +447,7 @@ class Agent:
         print('*')
         print("* Loaded configs from the file are:")
         print(configs)
+        print()
 
         algorithm = list(configs.keys())[0].upper()
         configs = configs[algorithm]
@@ -288,6 +515,9 @@ class Agent:
  
     def get_model(self):
         return self.model
+
+    def set_env(self, env):
+        self.env = env
 
     def predict(self, obs, use_rules=False, deterministic=False, **kwargs):
         if self.model:
